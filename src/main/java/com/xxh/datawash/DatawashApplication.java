693cd14cf5aa37e0eb03aed19610adb1189f295e
MasterXXH <xiangxiaohan2013@gmail.com>
2019-01-20 10:32:25 +0800

    create

---
+++ src/main/java/com/xxh/datawash/DatawashApplication.java
@@ -0,0 +1,14 @@
+package com.xxh.datawash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatawashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatawashApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/xxh/datawash/DatawashApplicationTests.java
@@ -0,0 +1,17 @@
+package com.xxh.datawash;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DatawashApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
